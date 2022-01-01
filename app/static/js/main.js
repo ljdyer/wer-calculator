c880a22455292cb5e8054b2725dd3c8f91881068
ljdyer <74581995+ljdyer@users.noreply.github.com>
2022-01-01 12:00:09 +0000

    change back

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -114,7 +114,7 @@ function gotStream(stream) {
 
 function initAudio() {
     if (!navigator.getUserMedia)
-        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mediaDevices.getUserMedia;
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     if (!navigator.cancelAnimationFrame)
         navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
     if (!navigator.requestAnimationFrame)
