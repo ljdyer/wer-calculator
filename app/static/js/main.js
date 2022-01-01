e7ca09d9132e1161f51379aefa6d94089513016f
ljdyer <74581995+ljdyer@users.noreply.github.com>
2022-01-01 11:55:32 +0000

    update to navigator.mediaDevices.getUserMedia for firefox

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -114,7 +114,7 @@ function gotStream(stream) {
 
 function initAudio() {
     if (!navigator.getUserMedia)
-        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mediaDevices.getUserMedia;
     if (!navigator.cancelAnimationFrame)
         navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
     if (!navigator.requestAnimationFrame)
